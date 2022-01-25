3d5a6bfd3b2c212062f37a057a218f9d771b7050
aimore <aimore.dutra@globality.com>
2022-01-25 14:23:26 +0000

    Added relative path

--- setup.py
+++ setup.py
@@ -14,11 +14,15 @@ setup(
     author_email="aimore.dutra@globality.com",
     description="Reproduce tfidf",
     install_requires=[
+        "setuptools==58.0.4",
         "pandas==1.3.3",
         "scipy==1.7.1",
         "scikit-learn==1.0",
         "numpy==1.21.2",
         "ipykernel==6.7.0",
         "microcosm>=2.4.1",
+        "joblib>=0.15",
+        "dvc",
+        "dvc[s3]",
     ],
 )
